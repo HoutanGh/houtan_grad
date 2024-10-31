dcc98a3ea84558e3ae526c4b4cb7532ae8b7c243
HoutanGh <houtan.ghaebi@gmail.com>
2024-10-31 20:19:56 +0000

    Activations dic

--- houtan_grad/neural_network.py
+++ houtan_grad/neural_network.py
@@ -13,7 +13,11 @@ class Library:
     
     def parameters(self):
         return []
-    
+
+ACTIVATIONS = {
+        'relu': lambda x: x.relu(),
+        'tanh': lambda x: x.tanh()
+        }
     
 class Neuron(Library):
     def __init__(self, n_in, non_lin=''):
@@ -24,13 +28,7 @@ class Neuron(Library):
     def __call__(self, x):
         act = sum((w_i * x_i for w_i, x_i in zip(self.w, x)), self.b) # sum takes second argument that is where it should start from
         # raw activation defined and now need to pass through non-linearity
-        if self.non_lin == 'relu':
-            return act.relu()
-        elif self.non_lin == 'tanh':
-            return act.tanh()
-        else:
-            return act
-    
+        return ACTIVATIONS.get(self.non_lin, lambda x: x)(act)    
     # want to be able to carry out actions on all the parameters
     def parameters(self):
         return self.w +[self.b]
@@ -72,4 +70,16 @@ class MLP(Library): #multi-layer perceptron
     
     def __repr__(self):
         return f"MLP: [{', '.join(str(layer) for layer in self.layers)}]"
-        
+
+class SGD:
+    def __init__(self, parametersm lr=0.01):
+        self.parameters = parameters
+        self.lr = lr
+
+    def step(self):
+        for param in self.parameters:
+            param.data -= self.lr * param.grad
+
+    def zero_grad(self):
+        for param in self.parameters:
+            param.grad = 0
