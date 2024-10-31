41a77debfedb41d7c399a8bf7330d9e45e597afc
HoutanGh <houtan.ghaebi@gmail.com>
2024-10-31 19:46:21 +0000

    Renaming

---
+++ houtan_grad/neural_network.py
@@ -0,0 +1,75 @@
+import random
+from my_grad.value import Value
+
+
+# neural network library
+
+# library class for defining zero_grad
+
+class Library:
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+    
+    def parameters(self):
+        return []
+    
+    
+class Neuron(Library):
+    def __init__(self, n_in, non_lin=''):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(n_in)] # wx_i + b for i inputs
+        self.b = Value(random.uniform(-1, 1))
+        self.non_lin = non_lin
+    
+    def __call__(self, x):
+        act = sum((w_i * x_i for w_i, x_i in zip(self.w, x)), self.b) # sum takes second argument that is where it should start from
+        # raw activation defined and now need to pass through non-linearity
+        if self.non_lin == 'relu':
+            return act.relu()
+        elif self.non_lin == 'tanh':
+            return act.tanh()
+        else:
+            return act
+    
+    # want to be able to carry out actions on all the parameters
+    def parameters(self):
+        return self.w +[self.b]
+    
+    def __repr__(self):
+        return f"{self.non_lin.capitalize()}Neuron({len(self.w)})"
+    
+
+class Layer(Library):
+
+    # adding **kwargs so that we can initialise neurons within Layers of the MLP with the activation functions defined as well as no activation function
+    def __init__(self, n_in, n_out, **kwargs): # not just using non_lin so that we don't have a default activation function
+        self.neurons = [Neuron(n_in, **kwargs) for _ in range(n_out)] # creates the number of neurons to match n_out?
+
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+    
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+
+class MLP(Library): #multi-layer perceptron
+    def __init__(self, n_in, n_outs, non_lin=''):
+        size = [n_in] + n_outs # outs are already a list
+
+        self.layers = [Layer(size[i], size[i+1], non_lin = non_lin) for i in range(len(n_outs))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in  layer.parameters()]
+    
+    def __repr__(self):
+        return f"MLP: [{', '.join(str(layer) for layer in self.layers)}]"
+        
---
+++ houtan_grad/value.py
@@ -0,0 +1,126 @@
+import math
+
+class Value:
+    def __init__(self, data, _children=(), _op='', label=''):
+        # Want to keep track of all the data of values that make current data, so introduce _children
+
+        self.data = data
+        self._prev = set(_children) # This is for efficiency, removing duplicates
+        self._op = _op
+        self.label = label
+        self._backward = lambda: None
+        self.grad = 0
+    
+    def __repr__(self) -> str:
+        return f"Value(data={self.data}, grad={self.grad})"
+    
+    def __add__(self, other):
+        # need to make a case for non-value objects
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward # assigning this nested function to the self._backward from __init__
+        
+        return out
+    
+    def __pow__(self, other):
+        assert isinstance(self, (int, float)) # accomodating for non-float can be difficult
+        out = Value(self.data**other, (self, ), f"**{other}")
+
+        def _backward():
+            self.grad += (other * self.data**(other - 1)) * out.grad
+        out._backward = _backward
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def __rmul__(self, other): # if can't do 2 * a, python will check if it can do a * 2
+        return self * other
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad += (1 - out.data**2) * out.grad # using t**2 is probably faster
+        out._backward = _backward
+
+        return out
+    
+    # another popular activation fucntion, defined a the positive part of its argument
+
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self, ), 'ReLU')
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad # returns 0 if in the negative part of the relu function
+        out._backward = _backward
+        return out
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), op='exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build(node):
+            if node not in visited:
+                visited.add(node)
+                for child in node._prev:
+                    build(child)
+                topo.append(node)
+        build(self)
+
+        self.grad = 1.0
+        for x in reversed(topo):
+            x._backward()
+
+    def __neg__(self):
+        return self * -1
+    
+    def __radd__(self, other):
+        return self + other
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __rsub__(self, other):
+        return other + (-self)
+    
+    def __rmul__(self, other):
+        return self * other
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __rtruediv__(self, other):
+        return other * self**-1
+
+
+
+    
+    
+
+    
+    
+    
